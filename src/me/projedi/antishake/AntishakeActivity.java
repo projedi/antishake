61fa4919b340be4175f61892fdb3f3e174510617
Alexander Shabalin <shabalyn.a@gmail.com>
2012-10-27 20:32:50 +0400

    Initial commit

---
+++ src/me/projedi/antishake/AntishakeActivity.java
@@ -0,0 +1,15 @@
+package me.projedi.antishake;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AntishakeActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
