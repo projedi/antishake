4a318edd86fefd64edc780700eb7ab41bc93bd7d
Alexander Shabalin <shabalyn.a@gmail.com>
2012-11-25 23:31:32 +0400

    Refactoring of ShakeService

--- src/me/projedi/antishake/ShakeService.java
+++ src/me/projedi/antishake/ShakeService.java
@@ -14,62 +14,66 @@ import android.hardware.SensorManager;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class ShakeService extends Service
-                          implements SensorEventListener {
-   private SensorManager m_sensorManager;
-   private Sensor m_accelerometer;
-   private Sensor m_gyroscope;
-   float[] m_transform;
-
-   private TimerTask m_timerTask = new TimerTask() {
-      @Override
-      public void run() {
-         Intent i = new Intent("android.intent.action.MAIN");
-         i.putExtra("transform", m_transform);
-         sendBroadcast(i);
-      }
-   };
-
+/**
+ * Service calculates a transformation to minimize shaking of a view
+ *
+ * Uses intent "me.projedi.antishake.ACTION_SHAKE" with field "transform"
+ * which is float[3] where float[0] - x translate, float[1] - y translate,
+ * float[2] - rotatation.
+ */
+public class ShakeService extends Service {
+// TODO: Use intents for setting accuracy and transfer rate
    @Override
-   public void onSensorChanged(SensorEvent event) {
-      //TODO: Try to get some rotation data from just acceleration
-      if (event.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
-         m_transform[0] = -event.values[0];
-         m_transform[1] = -event.values[1];
-      } else if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
-         m_transform[2] = event.values[2];
-      }
-   }
-
-   @Override
-   public void onAccuracyChanged(Sensor sensor, int accuracy) { }
+   public IBinder onBind(Intent intent) { return mBinder; }
 
    @Override
    public void onCreate() {
-      m_sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-      //TODO: It uses API Level 9 sensor. Implement a fallback to pure accelerometer.
-      m_accelerometer = m_sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
-      //TODO: Make it possible to adjust the 3rd parameter
-      m_sensorManager.registerListener( this, m_accelerometer
-                                      , SensorManager.SENSOR_DELAY_UI);
-      m_gyroscope = m_sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
-      m_sensorManager.registerListener( this, m_gyroscope
-                                      , SensorManager.SENSOR_DELAY_UI);
-      m_transform = new float[3];
-      Timer timer = new Timer();
-      timer.schedule(m_timerTask,0,30);
+      mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+      // TODO: It uses API Level 9 sensor. Implement a fallback to pure accelerometer.
+      mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+      mGyroscope = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+      // TODO: Make it possible to adjust the 3rd parameter
+      mSensorManager.registerListener(mSensorListener, mAccelerometer, mSensorAccuracy);
+      mSensorManager.registerListener(mSensorListener, mGyroscope, mSensorAccuracy);
+      mTransform = new float[3];
+      (new Timer()).schedule(mTimerTask, 0, mTransferRateMilliseconds);
    }
 
    @Override
-   public void onDestroy() {
-      m_sensorManager.unregisterListener(this);
+   public void onDestroy() { mSensorManager.unregisterListener(mSensorEventListener); }
 
+   private SensorManager mSensorManager;
+   private Sensor mAccelerometer;
+   private Sensor mGyroscope;
+   private float[] mTransform;
+   // TODO: come up with shorter name
+   private long mTransferRateMilliseconds = 10;
+   private int mSensorAccuracy = SensorManager.SENSOR_DELAY_FASTEST;
+
+   private TimerTask mTimerTask = new TimerTask() {
+      @Override
+      public void run() {
+         Intent shakeIntent = new Intent("me.projedi.antishake.ACTION_SHAKE");
+         shakeIntent.putExtra("transform", mTransform);
+         sendBroadcast(shakeIntent);
+      }
+   };
+
+   private SensorEventListener mSensorListener = new SensorEventListener() {
+      @Override
+      public void onAccuracyChanged(Sensor sensor, int accuracy) { }
+
+      @Override
+      void onSensorChanged(SensorEvent event) {
+         //TODO: Try to get some rotation data from just acceleration
+         if (event.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
+            mTransform[0] = -event.values[0];
+            mTransform[1] = -event.values[1];
+         } else if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+            mTransform[2] = event.values[2];
+         }
+      }
    }
 
-   private final IBinder m_binder = new Binder();
-
-   @Override
-   public IBinder onBind(Intent intent) {
-      return m_binder;
-   }
+   private final IBinder mBinder = new Binder();
 }
